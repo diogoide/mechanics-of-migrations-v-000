93b5eaeb679dd67bf58bc1d312082a4d1bfb4b53
Diogo Ide <diogoide@gmail.com>
2018-10-22 23:56:32 +0000

    automatically backed up by learn

--- db/migrate/02_add_favorite_food_to_artists.rb
+++ db/migrate/02_add_favorite_food_to_artists.rb
@@ -1,5 +1,7 @@
 class AddFavoriteFoodToArtists < ActiveRecord::Migration
 
   def change
-    add_column :artists, :favorite_food, :string 
+    add_column :artists, :favorite_food, :string
   end
+  
+end
